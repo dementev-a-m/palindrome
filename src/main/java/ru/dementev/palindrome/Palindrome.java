2e572c5c3cc4a1c7fef9b64c6ceab189447af825
Anton Dementev <antdementev@gmail.com>
2018-02-20 18:53:46 +0300

    Написал алгоритм

---
+++ src/main/java/ru/dementev/palindrome/Palindrome.java
@@ -0,0 +1,58 @@
+package ru.dementev.palindrome;
+
+/**
+ * Created by adementev on 20.02.2018.
+ */
+public class Palindrome {
+    public static int check(int p) {
+        int count = 0;
+        int[] array = new int[10];
+        boolean check = true;
+        while (count < 20 && check) {
+            int copyP = p;
+            int i = 0;
+            while (p != 0) {
+                array[i] = p % 10;
+                p /= 10;
+                i++;
+            }
+            check = checkPalindrome(array, i);
+            if (!check) {
+                p = sum(array, copyP, i);
+                check = true;
+                count++;
+            } else
+                break;
+
+        }
+        return count;
+    }
+
+    private static int sum(int[] array, int copyP, int capacity) {
+        int rang = 1;
+        int mirror = 0;
+        for (int j = capacity - 1; j >= 0; j--) {
+            mirror += array[j] * rang;
+            rang *= 10;
+        }
+        return mirror + copyP;
+    }
+
+    private static boolean checkPalindrome(int[] array, int capacity) {
+        for (int i = 0; i < capacity--; i++) {
+            if (array[capacity] != array[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(2345));
+        System.out.println(check(65));
+        System.out.println(check(181));
+        System.out.println(check(189));
+        System.out.println(check(241415136));
+    }
+
+
+}
